7b3593cc873579cdfbfde34eec58a4aac737f05e
Evans Pauliuts <pauliutsevans@gmail.com>
2020-01-14 14:38:56 +0300

    update fix code

--- src/apis.js
+++ src/apis.js
@@ -9,3 +9,12 @@ export const fetchApis = () => (
 export const filterDataApisCount = (data, count, names) => {
     return Object.values(data).filter(el => el.answers[count] === names).length;
 };
+
+export const config = {
+    apiKey: "AIzaSyB0A5GZSO2KKLV9L1w1Gl1U39wBp2VQSn0",
+    authDomain: "simplesurvey-8d9e3.firebaseapp.com",
+    databaseURL: "https://simplesurvey-8d9e3.firebaseio.com",
+    projectId: "simplesurvey-8d9e3",
+    storageBucket: "simplesurvey-8d9e3.appspot.com",
+    messagingSenderId: "14399663105"
+};
