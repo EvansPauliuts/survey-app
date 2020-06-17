1e9ff6fde1956d5020c46f60eca13331f282060c
Evans Pauliuts <pauliutsevans@gmail.com>
2020-06-17 11:41:43 +0300

    fix load list

--- src/components/Result.js
+++ src/components/Result.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { orderBy } from 'lodash';
 import Total from './Total';
 import { fetchApis } from '../apis.js';
+import Emoji from "./Emoji";
 
 const Result = () => {
     const [list, setList] = useState([]);
@@ -13,27 +14,25 @@ const Result = () => {
     const sortList = orderBy(list, 'datetime', 'desc');
 
     return (
-        <div className='list'>
-            <Total />
-            <h2>View surveys</h2>
-            { list.length === 0 ? 
-                <h4 className='load'>Loading...</h4> 
-                : 
-                <ul>
-                    {
-                    Object.values(sortList).map(( list, index ) => {
-                        return (
-                        <li className="list__item" key={ index }>
-                            <p>Name: { list.name }</p>
-                            <span role="img" aria-label="imac">🖥 { list.answers.q1 }</span>
-                            <span role="img" aria-label="tv">📺 { list.answers.q2 }</span>
-                            <span role="img" aria-label="iphone">📱 { list.answers.q3 }</span>
-                            <span role="img" aria-label="cpu">🤖 { list.answers.q4 }</span>
-                        </li>
-                        )
-                    })
-                    }
-                </ul>
+        <div>
+            { list.length === 0 ?
+                <h2 className="load"><Emoji symbol="🤔"/>...</h2>
+                :
+                <div className='list'>
+                    <Total />
+                    <h2>View surveys</h2>
+                    <ul>
+                        { Object.values(sortList).map(( list, index ) => (
+                            <li className="list__item" key={ index }>
+                                <p>Name: { list.name }</p>
+                                <span role="img" aria-label="imac">🖥 { list.answers.q1 }</span>
+                                <span role="img" aria-label="tv">📺 { list.answers.q2 }</span>
+                                <span role="img" aria-label="iphone">📱 { list.answers.q3 }</span>
+                                <span role="img" aria-label="cpu">🤖 { list.answers.q4 }</span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             }
         </div>
     );
