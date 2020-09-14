1439b76b9acd39a16d2ceaac513bcf208c7b4ed4
Evans Pauliuts <pauliutsevans@gmail.com>
2020-09-14 16:40:15 +0300

    fix #1

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,29 @@ import Survey from './containers/Survey';
 import Result from './containers/Result';
 import Nav from './containers/Nav';
 import NotFound from './components/NotFound';
+import Load from './components/Load';
 
 import './App.css';
 
 const App = () => (
     <Router>
-        <div className="container">
-            <div className='iam'>
-                <div className='flex'>
-                    <div className='logo'/>
-                    <p>evanscode</p>
+        <React.Suspense fallback={<Load />}>
+            <div className="container">
+                <div className='iam'>
+                    <div className='flex'>
+                        <div className='logo'/>
+                        <p>evanscode</p>
+                    </div>
+                    <Nav />
                 </div>
-                <Nav />
+                {/*<img src="https://www.codewars.com/users/EvansCode/badges/large" alt="iam"/>*/}
+                <Switch>
+                    <Route exact path='/' component={Survey} />
+                    <Route path='/result' component={Result} />
+                    <Route component={NotFound} />
+                </Switch>
             </div>
-            {/*<img src="https://www.codewars.com/users/EvansCode/badges/large" alt="iam"/>*/}
-            <Switch>
-                <Route exact path='/' component={Survey} />
-                <Route path='/result' component={Result} />
-                <Route component={NotFound} />
-            </Switch>
-        </div>
+        </React.Suspense>
     </Router>
 );
 
