9b4225e8baf49542aa95b833a07023a1add38cec
Evans Pauliuts <pauliutsevans@gmail.com>
2020-09-14 15:49:37 +0300

    fix router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import Survey from './containers/Survey';
 import Result from './containers/Result';
 import Nav from './containers/Nav';
@@ -8,7 +8,7 @@ import NotFound from './components/NotFound';
 import './App.css';
 
 const App = () => (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Router>
         <div className="container">
             <div className='iam'>
                 <div className='flex'>
@@ -17,14 +17,14 @@ const App = () => (
                 </div>
                 <Nav />
             </div>
-            <img src="https://www.codewars.com/users/EvansCode/badges/large" alt="iam"/>
+            {/*<img src="https://www.codewars.com/users/EvansCode/badges/large" alt="iam"/>*/}
             <Switch>
                 <Route exact path='/' component={Survey} />
                 <Route path='/result' component={Result} />
                 <Route component={NotFound} />
             </Switch>
         </div>
-    </BrowserRouter>
+    </Router>
 );
 
 export default App;
